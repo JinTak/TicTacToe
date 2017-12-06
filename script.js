f04fc7305d931977a846f45a27cfbf0563a2f3df
JinTak <jtak23@gmail.com>
2017-12-06 16:53:22 -0700

    added jquery functionality

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
+
+    $clickCounter = 0;
+    $x = "X";
+    $o = "O";
+
+    // Set Marker
+    $('.data').click(function(){
+        if($clickCounter % 2 == 0){
+            $(this).html($x);
+            $clickCounter++;
+        } else {
+            $(this).html($o);
+            $clickCounter++;
+        }
+        
+    });
+
+    // Clear Board Function
     $('#clear').click(function(){
-        $('.data').html("");
+        $('.data').html("");   
     });
 });

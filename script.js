a517a6e746b197f131ca004b443b0da93fa124cf
JinTak <jtak23@gmail.com>
2017-12-06 18:39:58 -0700

    styling is done. need to add more game logic

--- script.js
+++ script.js
@@ -4,17 +4,41 @@ $(document).ready(function(){
     $x = "X";
     $o = "O";
 
+  
+    
+
     // Set Marker
     $('.data').click(function(){
-        if($clickCounter % 2 == 0){
-            $(this).html($x);
-            $clickCounter++;
-        } else {
-            $(this).html($o);
-            $clickCounter++;
+
+       
+
+
+        if($(this).html() == ""){
+            if($clickCounter % 2 == 0){
+                $(this).html($x);
+                if(($('#1').html() == $x && $('#2').html() == $x && $('#3').html() == $x) 
+                    || ($('#4').html() == $x && $('#5').html() == $x && $('#6').html() == $x) 
+                    || ($('#7').html() == $x && $('#8').html() == $x && $('#9').html() == $x) 
+                    || ($('#1').html() == $x && $('#2').html() == $x && $('#3').html() == $x) 
+                    || ($('#4').html() == $x && $('#5').html() == $x && $('#6').html() == $x) 
+                    || ($('#7').html() == $x && $('#8').html() == $x && $('#9').html() == $x) 
+                        
+                    ){
+
+                    alert('Player X Wins!');
+                }
+                $clickCounter++;
+            } else {
+                $(this).html($o);
+                if (1<0){
+                    alert('Player O Wins!');
+                }
+                $clickCounter++;
+            }
         }
-        
     });
+
+    
 
     // Clear Board Function
     $('#clear').click(function(){

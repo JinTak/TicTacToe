fc656fb2779c3ffe78e49be75fbf68bb80f7d79d
JinTak <jtak23@gmail.com>
2017-12-06 20:52:01 -0700

    added more javascript

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
                     // $('.data').removeClass('background-pink');
                     // $clickCounter = 0; 
 
-                }
+                    }
                 if($('#1').html() != "" && $('#2').html() != "" && $('#3').html() != "" && $('#4').html() != "" && $('#5').html() != "" && $('#6').html() != "" && $('#7').html() != ""&& $('#8').html() != ""&& $('#9').html() != ""){
                     $('#playerTurn').remove();
                     $('#gameOver').html("CAT'S GAME!");
